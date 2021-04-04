43abc1dba0490941fbd81de699bf1bdc4b710b19
tdddt <doto1116@ewhain.net>
2021-04-05 00:08:25 +0900

    3

--- color.js
+++ color.js
@@ -1,30 +1,35 @@
-var Body={
-  setColor: function (color){
-    /*body font color 바꾸기*/
-    document.querySelector('body').style.color = color;
-  },
-    /*body backgound color 바꾸기*/
-  setBackgroundColor: function (color){
-    document.querySelector('body').style.backgroundColor = color;
-  }
-}
-
 var Link={
   /*link font color바꾸기*/
   setColor: function(color){
-    var alist = document.querySelectorAll('a');
-    var i=0;
-    while(i < alist.length){
-        console.log(alist[i]);
-        alist[i].style.color = color;
-        i=i+1;
-    }
+  //   var alist = document.querySelectorAll('a');
+  //   var i=0;
+  //   while(i < alist.length){
+  //       console.log(alist[i]);
+  //       alist[i].style.color = color;
+  //       i=i+1;
+  //   }
+  //웹페이지에 있는 모든 a태그를 jQuery로 제어하겠다는 의미
+    $('a').css('color',color);
+  }
+}
+
+var Body={
+  /*body font color 바꾸기*/
+  setColor: function (color){
+    // document.querySelector('body').style.color = color;
+    $('body').css('color',color);
+  },
+
+  /*body backgound color 바꾸기*/
+  setBackgroundColor: function (color){
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor',color);
   }
 }
 
 function nightDayHandler(self){ //this의 값을 self로 해야 함.
   var target = document.querySelector('body');
-  /*클릭 시 night화면*/
+  /*클릭 시 night화면으로*/
   if(self.value==='night'){
     Body.setBackgroundColor('black');
     Body.setColor('white');
@@ -32,7 +37,7 @@ function nightDayHandler(self){ //this의 값을 self로 해야 함.
 
     Link.setColor('pink');
 
-  } else { /*클릭 시 day화면*/
+  } else { /*클릭 시 day화면으로*/
     Body.setBackgroundColor('white');
     Body.setColor('black');
     self.value='night';
